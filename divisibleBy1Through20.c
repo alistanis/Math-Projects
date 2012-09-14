12e041c7062fb46a99227c16202c5384d447a943
Chris <christopher.michael.cooper@gmail.com>
2012-09-13 21:19:44 -0400

    Find the smallest number that is divisible by 1-20 evenly.

---
+++ divisibleBy1Through20.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <math.h>
+
+
+void check(int *x, bool *isAnswer);
+
+int main (int argc, const char * argv[])
+{
+	
+	int x = 20;
+    
+    bool isAnswer = false;
+	
+	while (isAnswer == false)
+	{
+		check(&x, &isAnswer);
+		
+		
+	}
+	printf("The smallest number evenly divisible by 1-20 is: %d \n", x);
+
+}
+
+void check (int *x, bool *isAnswer) {
+
+    
+	int i;
+    for(  i = 1; i<20; i++) 
+    {
+        if 
+        	( *x % i != 0 ) 
+        { 
+        	*isAnswer = false; 
+        	*x+=20;
+        	break; 
+        }
+        else
+        	*isAnswer = true;
+        
+    }
+	
+}
+
