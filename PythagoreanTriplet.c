b7360684cc3362774f973232fe17d906e64c503a
Chris <christopher.michael.cooper@gmail.com>
2012-09-14 13:34:47 -0400

    Changed the pythagorean triplet to long long, and added user input to find any number instead of just 1000.

--- PythagoreanTriplet.c
+++ PythagoreanTriplet.c
@@ -1,26 +1,34 @@
 #include <stdio.h>
-
+#include <stdbool.h>
 
 int main (int argc, const char * argv[])
 {
-int i=1, j=1, k=1, result;
+long long i=1, j=1, k=1, result;
+long long input;
+bool IsTriplet = false;
+printf("Please enter an integer to check if it is the result of a Pythagorean triplet, and if so, calculate the product of that result. \n");
+scanf("%lld", &input);
 
-for (i; i<1000; i++)
+for (i; i<input; i++)
 {
-	for (j=1; j<1000; j++)
+	for (j=1; j<input; j++)
 	{
-		k = 1000-i-j;
+		k = input-i-j;
 		if (i*i+j*j == k*k)
 		{
 			result = i*j*k;
-			printf("a = %d, b = %d, c = %d \n", i, j, k);
-			printf("Product of the only pythagorean triplet to sum to 1000: %d \n", result);
-			
+			printf("a = %lld, b = %lld, c = %lld \n", i, j, k);
+			printf("Product of the only Pythagorean triplet to sum to %lld: %lld \n", input, result);
+			IsTriplet = true;
 		}
 		if (i*i+j*j == k*k)
 			break;
 	}
 	if (i*i+j*j == k*k)
-		break;
+		break;	
+}	
+if (IsTriplet == false)
+{
+	printf("No triplet exists! \n");
 }
 }
